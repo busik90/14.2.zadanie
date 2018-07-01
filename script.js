f701341a34414842fb65df45baf9343875038c07
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-01 12:39:48 +0200

    Add covers to films

--- script.js
+++ script.js
@@ -2,29 +2,34 @@ var movies = [
   {
     id: 1,
     title: 'Harry Potter',
-    desc: 'Film o czarodzieju'
+    desc: 'Film o czarodzieju',
+    cover: 'harry-potter.jpg'
   },
   {
     id: 2,
     title: 'Król Lew',
-    desc: 'Film o królu sawanny'
+    desc: 'Film o królu sawanny',
+    cover: 'król-lew.jpg'
   },
   {
     id: 3,
     title: 'Atlas Chmur',
-    desc: 'Film o życiach ludzi w różnych czasach, różnych wcieleniach'
+    desc: 'Film o życiach ludzi w różnych czasach, różnych wcieleniach',
+    cover: 'atlas-chmur.jpg'
   },
   {
     id: 4,
     title: 'Colony',
-    desc: 'Serial o najeźdźcach z kosmosu'
+    desc: 'Serial o najeźdźcach z kosmosu',
+    cover: 'colony.jpg'
   }
 ];
 
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.desc)
+      React.createElement('p', {}, movie.desc),
+      React.createElement('img', {src: "./images/" + movie.cover})
     );
 });
 
