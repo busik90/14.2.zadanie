674cd54004ff9f8a4799e0bc5318a91a50f9af52
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-01 12:05:35 +0200

    Add files with basic code

---
+++ script.js
@@ -0,0 +1,27 @@
+var movies = [
+  {
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju'
+  },
+  {
+    id: 2,
+    title: 'Król Lew',
+    desc: 'Film o królu sawanny'
+  }
+];
+
+var moviesElements = movies.map(function(movie) {
+  return React.createElement('li', {key: movie.id},
+      React.createElement('h2', {}, movie.title),
+      React.createElement('p', {}, movie.desc)
+    );
+});
+
+var element =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements)
+  );
+  
+ReactDOM.render(element, document.getElementById('app'));
